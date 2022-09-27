e3786555945b291c983753ac91f6175fc5e5bcc0
farhan <farhanarrahman44@gmail.com>
2022-09-27 17:02:26 +0700

    initial

---
+++ API/Controllers/DivisionController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+using API.Context;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DivisionController : Controller
+    //public class DivisionController : ControllerBase
+    {
+        MyContext myContext;
+
+        public DivisionController(MyContext myContext)
+        {
+            this.myContext = myContext;
+        }
+
+        // READ
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = myContext.Divisions.ToList();
+            if (data.Count == 0)
+                return Ok(new { message = "sukses mengambil data", statusCode = 200, data = "null" });
+            return Ok(new { message = "sukses mengambil data", statusCode = 200, data = data });
+
+            //return BadRequest(new { message = "sukses mengambil data", statusCode = 200, data = data });
+            //return NotFound(new { message = "sukses mengambil data", statusCode = 200, data = data });
+            //return Unauthorized(new { message = "sukses mengambil data", statusCode = 200, data = data });
+            //return Forbid(new { message = "sukses mengambil data", statusCode = 200, data = data });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var data = myContext.Divisions.Find(id);
+            if(data == null)
+                return Ok(new { message = "sukses mengambil data", statusCode = 200, data = "null" });
+            return Ok(new { message = "sukses mengambil data", statusCode = 200, data = data });
+        }
+
+        // UPDATE
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Division division) // /api/Division/{id}
+        //public IActionResult Put(Division division) // /api/Division
+        {
+
+            var data = myContext.Divisions.Find(id); // /api/Division/{id}
+            //var data = myContext.Divisions.Find(division.Id); /api/Division
+            data.Name = division.Name;
+            myContext.Divisions.Update(data);
+            var result = myContext.SaveChanges();
+            if (result > 0)
+                return Ok(new { statusCode = 200, message = "berhasil mengupdate data" });
+            return BadRequest(new { statusCode = 400, message = "gagal mengupdate data" });
+        }
+
+        // CREATE
+        [HttpPost]
+        public IActionResult Post(Division division)
+        {
+            myContext.Divisions.Add(division);
+            var result = myContext.SaveChanges();
+            if (result > 0)
+                return Ok(new { statusCode = 200, message = "berhasil menambahkan data" });
+            return BadRequest(new { statusCode = 400, message = "gagal menambahkan data" });
+        }
+
+        // DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var data = myContext.Divisions.Find(id);
+            myContext.Divisions.Remove(data);
+            var result = myContext.SaveChanges();
+            if(result > 0)
+                return Ok(new { statusCode = 200, message = "berhasil menghapus data" });
+            return BadRequest(new { statusCode = 400, message = "gagal menghapus data" });
+        }
+
+        //// GET: api/values
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
